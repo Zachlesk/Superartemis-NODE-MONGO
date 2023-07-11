acc52d905fbc2379f47a284f68dc6d3fc7ba21fa
Zachlesk <zharojas0723@gmail.com>
2023-07-11 11:29:49 -0500

    feat: :sparkles: metodos https

---
+++ backend/routes/categorias.routes.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const router = express.Router();
+
+import {obtenerCategorias, agregarCategoria, borrarCategorias, actualizarCategorias} from "../controllers/categoria.controllers.js"
+
+router.get("/all", obtenerCategorias);
+router.post("/add", agregarCategoria);
+router.delete("/remove/:id", borrarCategorias);
+router.patch("/update/:id", actualizarCategorias)
+
+export default router;
